dee597084454535d8b27a9840d63f494242a5df5
H8inSIDE <gaziba92@hotmail.com>
2021-07-23 09:56:07 +0100

    Add swagger-ui

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,11 +1,11 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-	@GetMapping
+	@GetMapping("/demo")
 	public boolean get() {
 		return true;
 	}
